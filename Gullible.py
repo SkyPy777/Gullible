bf19e5fff2ad6bce7b2a934c9b2d75a120c384ac
Subhash Kumar yadav <141063498+SkyPy777@users.noreply.github.com>
2023-10-11 09:07:29 +0530

    Add files via upload

---
+++ Gullible.py
@@ -0,0 +1,13 @@
+def gullible_bot():
+
+    
+        print("Hello, i am gullible bot, you can say me anything. ")
+        while True:
+            user_input = input("Say me anything:")
+            print(user_input)
+            print(f"This is how i keep people busy {user_input}")
+
+            gullible_bot()
+
+
+
